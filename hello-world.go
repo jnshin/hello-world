109d2890760248ab93f26234428dad78fa00c304
Jaenyun <tlswosus@gmail.com>
2017-05-17 18:25:51 +0900

    Add comment.

--- hello-world.go
+++ hello-world.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("안녕~!")
 	fmt.Println("2017.05.17 16시")
-	fmt.Printf("현재 - %d\n", PopCount(1234567890))
+	fmt.Printf("현재 - %d\n", PopCount(1234567890)) // test code 추가.
 }
