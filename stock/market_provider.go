69aa31fe2f02073c81a49d7b6e437b773b5e5b69
Jaenyun Shin <tlswosus@gmail.com>
2019-01-02 18:03:05 +0900

    changing marketprovider

--- stock/market_provider.go
+++ stock/market_provider.go
@@ -1,18 +1,21 @@
 package stock
 
-/*
 import (
 	"fmt"
 )
-*/
+
 
 // MarketProvider 는 stock quote를 제공하는 provider의 기본 기능을 정의한다
 // naver 또는 yahoo와 같이 data source에 따라 구현을 달리한다.
+// ToDo : 정보제공 범위에 대한 config 정보를 표현할 방법 필요.
+// 예를들어 어디는 수정주가를 제공하고 어디는 아닌 값을 제공한다거나... 
+// 어디는 history 값을 가져올 방법이 없다거나.... 
+// 장기적으로는 반복적인 GetQuote 를 너무 빈번하게 호출하지 못하도록 제한할 필요 있다. 
 type MarketProvider interface {
 
-	// getQuote : 조회하고자하는 stock을 생성해 전달
+	// GetQuote : 조회하고자하는 stock을 생성해 전달
 	//   종목을 구분하기 위한 최소한의 정보가 포함되어야 함. 예) 종목코드
-	quote(i Stock) (Stock, error)
+	GetQuote(i *Stock) (*Stock, error)
 
 	// quoteList : 상장종목 리스트를 반환. 단 가격정보는 포함하지 않는다
 	quoteList() (s []Stock)
@@ -25,4 +28,24 @@ type MarketProvider interface {
 
 	// providerName : data provider 이름을 반환한다.
 	providerName() (name string)
+
 }
+
+// Naver Stock 
+
+type NaverStock struct {}
+
+func (p *NaverStock) GetQuote(s *Stock) (*Stock, error) {
+	fmt.Printf("구현해라. NaverStock.GetQuote")
+	return s, nil
+}
+
+// Yahoo Finance 
+
+type YahooFinance struct {}
+
+func (p *YahooFinance) GetQuote(s *Stock) (*Stock, error) {
+	fmt.Printf("구현해라. YahooFinance.GetQuote")
+	return s, nil
+}
+
--- stock/stock.go
+++ stock/stock.go
@@ -11,7 +11,7 @@ type Stock struct {
 	Code          int      // 종목코드
 	URL           *url.URL // data source URL
 	Name          string   // 종목명
-	Market        string   // 코스피 / 코스닥
+	ProviderContext MarketProvider   // 코스피 / 코스닥
 	Price         int      // 현재가 또는 종가
 	YPrice        int      // 전일가
 	Open          int      // 시가
@@ -24,5 +24,10 @@ type Stock struct {
 
 // ToString 은 Stock 의 기본 정보를 string으로 전환한다.
 func (e *Stock) ToString() string {
-	return fmt.Sprintf("%s(%d, %s) : 현재가 %v원, 가격폭 %v~%v 상하한[%v,%v] 거래량 %v주\n", e.Name, e.Code, e.Market, e.Price, e.DayLow, e.DayHigh, e.DayLowerLimit, e.DayUpperLimit, e.Volume)
+	return fmt.Sprintf("%s(%d, %s) : 현재가 %v원, 가격폭 %v~%v 상하한[%v,%v] 거래량 %v주\n", e.Name, e.Code, e.ProviderContext, e.Price, e.DayLow, e.DayHigh, e.DayLowerLimit, e.DayUpperLimit, e.Volume)
+}
+
+func (e *Stock) GetQuote() *Stock {
+	e.ProviderContext.GetQuote(e)
+	return e
 }
